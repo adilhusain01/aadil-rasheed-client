9d4f498ceef1dcf6c829c851d5a76306b4d602aa
Adil husain <husainadil202@gmail.com>
2025-03-18 01:59:49 +0530

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   distDir: "build",
+//   images: {
+//     unoptimized: true,
+//     domains: ["res.cloudinary.com"],
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   distDir: "build",
   images: {
     unoptimized: true,
+    domains: ["res.cloudinary.com"],
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/djxuqljgr/",
   },
   typescript: {
     ignoreBuildErrors: true,
